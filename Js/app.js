9945d7eb2f07cb19644f2015bfda738f518a9b5f
ahnafhemal <hemalhasan140@gmail.com>
2023-03-21 23:17:41 +0600

    dom

---
+++ Js/app.js
@@ -0,0 +1,9 @@
+const numberButton = document.getElementsByClassName('numbers')
+const operationButton = document.getElementsByClassName('opetarion')
+const equalsButton = document.getElementById('btn-equals')
+const deleteButton = document.getElementById('btn-delete')
+const allClearButton = document.getElementById('btn-all-clear')
+const previousOperandButton = document.getElementById('data-previous-operand')
+const currentOperandButton = document.getElementById('data-current-operand')
+
+
